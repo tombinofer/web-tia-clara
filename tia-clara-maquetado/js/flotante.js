a073e0d63a92e83349f4873d8193e8e7f869b6ba
tombinofer <fernandogorozco@gmail.com>
2014-11-13 14:17:05 -0300

    cambio de imagenes

--- tia-clara-maquetado/js/flotante.js
+++ tia-clara-maquetado/js/flotante.js
@@ -7,7 +7,6 @@ $( document ).ready(function(){
     if (scroll > 100) {
         $('.nav .flotante').fadeIn();
         $('.nav .flotante li a').css("color","black");
-        $('.nav .flotante li a:hover').css("color","#bab03e");
 
     } else {
         $('.nav .flotante').hide();
