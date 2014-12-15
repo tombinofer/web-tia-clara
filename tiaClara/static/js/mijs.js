457d07a1b1d23fbc64f00ff33bb7ffba4746e6c1
tombinofer <fernandogorozco@gmail.com>
2014-12-15 18:03:31 -0300

    agregando google analytics

--- tiaClara/static/js/mijs.js
+++ tiaClara/static/js/mijs.js
@@ -37,7 +37,56 @@ $( document ).ready(function(){
 */
     /////////para el Footer!
 
-    
+
+
+
+/*$(window).scroll(function (event) {
+        var position_nosotros = $('#inicio').position();
+        var position_mermeladas = $('#mermeladas').position();
+        var position_dulces = $('#dulces').position();
+        var position_jaleas = $('#jaleas').position();
+        var position_otros = $('#otros').position();
+        var position_contacto = $('#contacto').position();
+
+        if (posicion == position_nosotros) {
+            $('.menu .selected').removeClass('selected');
+            $(this).addClass('.activo');
+        } 
+        
+        if(posicion == position_mermeladas){
+            $('.menu .selected').removeClass('selected');
+            $(this).addClass('activo');
+        }
+
+        if(posicion == position_dulces){
+            $('.menu .selected').removeClass('selected');
+            $(this).addClass('activo');
+        }
+        if(posicion == position_jaleas){
+            $('.menu .selected').removeClass('selected');
+            $(this).addClass('activo');
+        }
+        if(posicion == position_otros){
+            $('.menu .selected').removeClass('selected');
+            $(this).addClass('activo');
+        }
+        if(posicion == position_contacto){
+            $('.menu .selected').removeClass('selected');
+            $(this).addClass('activo');
+        }
+    });
+
+*/
+
+
+    $("#menu_principal li a").click(function () {
+          $("#menu_principal li a").removeClass(".selected");    
+          $(this).addClass(".selected");
+    });
+  
+
+
+
 
 
 
